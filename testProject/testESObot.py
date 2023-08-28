68d42642154795e2da17392eabdd198f77547dc0
James <wallacecjames99@gmail.com>
2023-08-28 21:42:17 +0100

    complete random initialisation methods in esobot

--- testProject/testESObot.py
+++ testProject/testESObot.py
@@ -12,19 +12,17 @@ class esoBot():
         self.generator_assets = []
         # if mode isn't random need to allow users to pass a custom list of generator, load and storage assets
     
-    def generate_storage_random(self, num_assets: int, output_range: list, max_capacity_range:list, current_range: list):
-        for i in range(num_assets):
-            self.storage_assets.append(energyStorage(name = "storage" + str(i), maxOutput=random.randint(output_range[0], output_range[1]), state = 'static', maxCapacity=random.randint(max_capacity_range[0], max_capacity_range[1]), MaxCRate=1, currentCapacity=random.randint(current_range[0], current_range[1])))
-            
-        return(self.storage_assets)
+    def generate_storage(self, name: str, output: float, max_capacity: float, current_capacity: float):
+        self.storage_assets.append(energyStorage(name = name, maxOutput=output, state = 'static', maxCapacity= max_capacity, MaxCRate=1, currentCapacity= current_capacity))
+        return()
     
-    def generate_load_random(self, num_assets: int, max_load_range: list, load_time_limit_range: list):
-        for i in range(num_assets):
-            self.load_assets.append(energyLoad(name = "load" + str(i), maxLoad=random.randint(max_load_range[0], max_load_range[1]), loadTimeLimit=random.uniform(load_time_limit_range[0], load_time_limit_range[1])))
+    def generate_load(self, name: str, max_load: float, load_time_limit: float):
+        self.load_assets.append(energyLoad(name = name, maxLoad=max_load, loadTimeLimit= load_time_limit))
+        return()
     
     def generate_generator(self, name: str, init_power: float):
         self.generator_assets.append(Solar(name = name, power = init_power))
-        return(self.generator_assets)
+        return()
     
     def initiate_grid(self, mode: str, asset_csv_pathname):
         
@@ -41,35 +39,27 @@ class esoBot():
                 
                 for j in range(row["num_assets"]):
                     
-                    if asset_types[i] == "generator":
-                        if mode == 'random':
-                            if type(row['name']) == str: # np.isnan(row['name']) is False
-                                temp_name = row['name'] + "_" + str(j)
-                            else:
-                                temp_name = "generator_" + str(j)
-            
+                    if mode == 'random':
+                        if type(row['name']) == str:
+                            temp_name = row['name'] + "_" + str(j)
+                        else:
+                            temp_name = asset_types[i] + "_" + str(j)
+                    
+                        if asset_types[i] == "generator":
                             temp_params = {"power": round(random.uniform(row["power_range_lower"], row["power_range_upper"]), 2)}
-                        
-                        self.generate_generator(name = temp_name, init_power = temp_params['power'])
-                        
-                        
-                        if mode == 'defined':
-                            pass
+                            self.generate_generator(name = temp_name, init_power = temp_params['power'])
                             
-                    if asset_types[i] == "load":
+                        if asset_types[i] == "load":
+                            temp_params = {"max_load": round(random.uniform(row["max_load_range_lower"], row["max_load_range_upper"]), 2), "load_time_limit": round(random.uniform(row["load_time_limit_range_lower"], row["load_time_limit_range_upper"]), 2)}
+                            self.generate_load(name = temp_name, max_load= temp_params['max_load'], load_time_limit= temp_params['load_time_limit'])
+                        
+                        if asset_types[i] == "storage":
+                            temp_params = {"max_output": round(random.uniform(row['output_range_lower'], row['output_range_upper']), 2), "max_capacity": round(random.uniform(row['max_capacity_range_lower'], row['max_capacity_range_upper']),2 ), "current_capacity": round(random.uniform(row['current_range_lower'], row['current_range_upper']), 2)}
+                            self.generate_storage(name = temp_name, output=temp_params["max_output"], max_capacity=temp_params["max_capacity"], current_capacity=temp_params["current_capacity"])
+                    
+                    if mode == 'defined':
                         pass
-                    
-                    if asset_types[i] == "storage":
-                        pass
-            
-            
-            
-            pass # call the initate asset function
-                    
-                    
-        
-        # write this next, should account for mode and include/generate random params in random mode
-        # should create all the assets
+                
         pass 
     
     def run_grid():
--- testProject/test_eso_multi.py
+++ testProject/test_eso_multi.py
@@ -1,27 +1,14 @@
 from testESObot import esoBot
-from config import energyLoad, energyGenerator, energyStorage
 
 test_bot = esoBot()
 
-# test generators generation
-test_bot.generate_generators_random(num_assets=5, power_range=[50, 100])
+test_bot.initiate_grid(mode='random', asset_csv_pathname= "test_random_input.csv")
 
 for i in range(len(test_bot.generator_assets)):
     print(test_bot.generator_assets[i])
-    
-# test storage generation
-    
-test_bot.generate_storage_random(num_assets= 5, output_range= [50, 100], max_capacity_range= [100, 200], current_range= [0, 200])
 
+for i in range(len(test_bot.load_assets)):
+    print(test_bot.load_assets[i])
+    
 for i in range(len(test_bot.storage_assets)):
     print(test_bot.storage_assets[i])
-
-
-# test load generation
-
-test_bot.generate_load_random(num_assets= 5, max_load_range= [10, 100], load_time_limit_range= [0.01, 1])
-
-for i in range(len(test_bot.generator_assets)):
-    print(test_bot.load_assets[i])
-
-test_bot.initiate_grid(mode = "random", asset_csv_pathname="test")
