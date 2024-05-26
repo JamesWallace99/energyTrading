1c341b12163f9b8d634c8e60e6fcb93c141d185c
James <wallacecjames99@gmail.com>
2024-05-26 22:05:28 +0100

    clean up tests for esobot

--- testProject/test_new_ESObot.py
+++ testProject/test_new_ESObot.py
@@ -12,6 +12,9 @@ csv_filepath = "test_random_input.csv"
 # print(x.imbalance_profile)
 # x.get_imbalance_forecast(sim_length = 10, time_step= 1)
 
+test_time_step = 0.25
+test_sim_length = 240
 
-x.balance_grid(sim_length=10, time_step=1, asset_csv_pathname= csv_filepath)
-x.plot_imbalance(sim_length=10, time_step= 1)
+
+x.balance_grid(sim_length=test_sim_length, time_step=test_time_step, asset_csv_pathname= csv_filepath)
+x.plot_imbalance(sim_length=test_sim_length, time_step= test_time_step)
